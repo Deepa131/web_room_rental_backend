513afac7bb5af6ebc602138a01cd6492132cde16
Deepa131 <deepapaudel65@gmail.com>
2026-02-08 23:31:00 +0545

    updated dto

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -30,3 +30,19 @@ export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
 
 export const UpdateUserDTO = UserSchema.partial();
 export type UpdateUserDTO = z.infer<typeof UpdateUserDTO>;
+
+export const ForgotPasswordDTO = z.object({
+    email: z.email(),
+});
+export type ForgotPasswordDTO = z.infer<typeof ForgotPasswordDTO>;
+
+export const ResetPasswordDTO = z
+    .object({
+        password: z.string().min(6),
+        confirmPassword: z.string().min(6),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
+export type ResetPasswordDTO = z.infer<typeof ResetPasswordDTO>;
