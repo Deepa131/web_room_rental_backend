225391ae377ed2d89be0724ab3ba70e4ee843307
Deepa131 <deepapaudel65@gmail.com>
2026-02-26 00:12:46 +0545

    updated user type code

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,11 +1,11 @@
 import z from "zod";
 
 export const UserSchema = z.object({
-    fullName: z.string().min(1),
-    email: z.string().email(),
-    password: z.string().min(6),
+    fullName: z.string().min(1).trim(),
+    email: z.string().email().trim(),
+    password: z.string().min(6).trim(),
     role: z.enum(["renter", "owner", "admin"]).default("renter"),
-    profilePicture: z.string().optional(),
+    profilePicture: z.string().nullish().default("default-profile.png"),
 });
 
 export type UserType = z.infer<typeof UserSchema>;
